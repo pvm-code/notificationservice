aa476d1a6abaf29444b9b9a271332e69e8d695af
prashant <pvm.code@gmail.com>
2026-08-03 20:48:26 +0530

    Add JWT security to notification service

--- src/main/java/com/notificationservice/controller/TestController.java
+++ src/main/java/com/notificationservice/controller/TestController.java
@@ -1,5 +1,13 @@
 package com.notificationservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
 
+    @GetMapping("/test")
+    public String test() {
+        return "Notification Service - Protected Endpoint Accessed Successfully";
+    }
 }
